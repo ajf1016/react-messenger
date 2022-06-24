82597d72a713dd3a2d74124e7fe68940fe5669ab
ajf1016 <ajmal.fayiz1016@gmail.com>
2022-06-24 15:20:02 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -9,15 +9,25 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDh8CjWbQOCETBhbw5Tb2nH-UWMc0FfB7Q",
+//   authDomain: "firechat-bdea1.firebaseapp.com",
+//   databaseURL: "https://firechat-bdea1-default-rtdb.firebaseio.com",
+//   projectId: "firechat-bdea1",
+//   storageBucket: "firechat-bdea1.appspot.com",
+//   messagingSenderId: "192165734607",
+//   appId: "1:192165734607:web:c9e0f65fa7957dfa196fba",
+//   measurementId: "G-S4PTGTE795",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDh8CjWbQOCETBhbw5Tb2nH-UWMc0FfB7Q",
-  authDomain: "firechat-bdea1.firebaseapp.com",
-  databaseURL: "https://firechat-bdea1-default-rtdb.firebaseio.com",
-  projectId: "firechat-bdea1",
-  storageBucket: "firechat-bdea1.appspot.com",
-  messagingSenderId: "192165734607",
-  appId: "1:192165734607:web:c9e0f65fa7957dfa196fba",
-  measurementId: "G-S4PTGTE795"
+  apiKey: "AIzaSyBrAO4cdgZbYywiOncYN5sNwe1sAmT93Zg",
+  authDomain: "chatbase-927bc.firebaseapp.com",
+  databaseURL: "https://firechat-927bc-default-rtdb.firebaseio.com",
+  projectId: "chatbase-927bc",
+  storageBucket: "chatbase-927bc.appspot.com",
+  messagingSenderId: "405841228561",
+  appId: "1:405841228561:web:89596d754a4d172b2952b0",
 };
 
 // Initialize Firebase
